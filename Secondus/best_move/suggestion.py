84b9f7aa38435187011938b853b2cfdc330afcce
Tristan Peat <tristan.j.peat@gmail.com>
2022-10-22 19:08:33 -0400

    Package into function

--- Secondus/best_move/suggestion.py
+++ Secondus/best_move/suggestion.py
@@ -1,15 +1,27 @@
 from stockfish import Stockfish
 
-# fetch fen from CV
-fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+'''
+@param: fen string
+@returns: list of dict of top 3 best moves
 
-stockfish = Stockfish(path='/opt/homebrew/bin/stockfish')
-# larger the hash value better, keep power of 2
-stockfish.update_engine_parameters({"Hash": 2048})
+Dict structure is as such
+{'Move': 'd2d4', 'Centipawn': 41, 'Mate': None}
 
-# check if fen is valid
-if stockfish.is_fen_valid(fen):
-    # sets the position of fen
-    stockfish.set_fen_position(fen)
-    # 
-    print(stockfish.get_top_moves(3))
+FEN tells you whose move it is: notice the 'w' stands for White's turn, 'b' stands for Black's turn
+'''
+def top_moves(fen):
+    # init stockfish engine
+    stockfish = Stockfish()
+    # larger the hash value better, keep power of 2
+    stockfish.update_engine_parameters({"Hash": 2048})
+
+    # check if fen is valid
+    if stockfish.is_fen_valid(fen):
+        # sets the position of fen
+        stockfish.set_fen_position(fen)
+        # return list of the top 3 moves
+        return stockfish.get_top_moves(3)
+
+test_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+print(top_moves(test_fen))
+
